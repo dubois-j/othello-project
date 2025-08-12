fdf256f98746bcea6d25db8b06043aa34ff57af6
J. Dubois <julienhm.dubois@gmail.com>
2025-08-12 12:08:57 +0200

    feat: Check if flip is possible

--- othello/board.py
+++ othello/board.py
@@ -44,6 +44,14 @@ class Board:
         If the move is valid, adds a Pawn of given color to the given position.
         """
 
+        # Verifying input validity
+        if color not in [0,1]:
+            raise ValueError(f"Color must be either 0 or 1 but {color} was given instead.")
+        
+        if row>7 or row<0 or col>7 or col<0:
+            raise ValueError(f"Column and row index must be contained between (0,0) and (7,7) but ({row}, {col}) was given instead.")
+        
+        # Add Pawn
         if self.canAddPawn(row, col, color):
             self.board[row, col] = Pawn(color)
             self.flipPawns(row, col, color)
@@ -63,11 +71,16 @@ class Board:
         Returns:
             (Bool): True if valid move, False otherwise.
         """
-
-        if self.isSquareEmpty(row, col) and self.isFlipPossible(row, col, color):
-            return True
-        else:
+        
+        # Checking if a Pawn can be added
+        if not self.isSquareEmpty(row, col) and 
             return False
+        
+        for direction in ['N','NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']:
+            if not self.isFlipPossible(row, col, color, direction):
+                return False
+        
+        return True
 
 
     def isSquareEmpty(self, row, col):
@@ -82,6 +95,7 @@ class Board:
             (Bool): True if Square is empty, False otherwise.
         """
 
+        # Checking if a square is empty
         if self.board[row, col].pawn == None:
             return True
         else:
@@ -103,8 +117,84 @@ class Board:
             (Bool): True if at least one Pawn will be flipped, False otherwise.
         """
 
-        pass
+        # Verifying input validity
+        if color not in [0,1]:
+            raise ValueError(f"Color must be either 0 or 1 but {color} was given instead.")
+        
+        if row>7 or row<0 or col>7 or col<0:
+            raise ValueError(f"Column and row index must be contained between (0,0) and (7,7) but ({row}, {col}) was given instead.")
+        
+        # Check if can flip direction, stops as soon as flip possible in one direction
+        directions = ['N','NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+        canFlip = False
+        i_dir = -1
+
+        while not canFlip and i_dir < len(directions):
+            direction = directions[i_dir]
+            increments = self.directionToIndexIncrement(direction)
+            current_row, current_col = row + increments[0], col + increments[1]
+            stillInBoard = True
+            nextIsEmpty = False
+            nextIsSameColor = False
+
+            ## First loop, necessary because can't flip on first iteration no matter what
+            # Check if not within board
+            if not (0 <= current_row <= 7 and 0 <= current_col <= 7):
+                stillInBoard = False
+            
+            # Else, check if next is empty
+            elif self.board[current_row, current_col].pawn==None:
+                nextIsEmpty = True
+            
+            # Else, check if next is same color
+            elif self.board[current_row, current_col].pawn.color == color:
+                nextIsSameColor = True
+            
+
+            # Parse direction until done
+            while stillInBoard and not nextIsEmpty and not nextIsSameColor and not canFlip:
+                current_row, current_col = row + increments[0], col + increments[1]
+
+                # Check if not within board
+                if not (0 <= current_row <= 7 and 0 <= current_col <= 7):
+                    stillInBoard = False
+                
+                # Else, check if next is empty
+                elif self.board[current_row, current_col].pawn==None:
+                    nextIsEmpty = True
+                
+                # Else, check if next is same color
+                elif self.board[current_row, current_col].pawn.color == color:
+                    nextIsSameColor = True
+                    canFlip = True
+
+            i_dir += 1
+            
+        return canFlip
     
+    def directionToIndexIncrement(self, direction):
+        """
+        Transforms a direction in ['N','NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'] to a index increment to parse the board.
+
+        Args:
+            direction (str): string from ['N','NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+        
+        Returns:
+            (tup): tuple of index increment (row_increment, col_increment)
+        """
+        
+        row_increment, col_increment = 0, 0
+        if 'N' in direction:
+            row_increment = -1
+        if 'S' in direction:
+            row_increment = 1
+        if 'E' in direction:
+            col_increment = 1
+        if 'W' in direction:
+            col_increment = -1
+        
+        return (row_increment, col_increment)
+
 
     def flipPawns(row, col, color):
         pass
