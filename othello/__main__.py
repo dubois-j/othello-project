124a23f34523e3612b498d73e7d85f04b9f3773b
Thibault Launois <thibault.launois@posteo.net>
2025-08-12 12:32:05 +0200

    feat: 1st structure for main

--- othello/__main__.py
+++ othello/__main__.py
@@ -1,1 +1,11 @@
-print("Ca marche !")
+import othello.board as board
+import othello.player as player
+import othello.othello as othello
+
+game=othello()
+
+while not game.isGameOver():
+    othello.play()
+
+game.showResults()
+
