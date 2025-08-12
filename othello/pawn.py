cf391a5e5a56f1ef873c254d7f424640a2de9f97
Thibault Launois <thibault.launois@posteo.net>
2025-08-12 09:50:53 +0200

    Create docstr for pawn

--- othello/pawn.py
+++ othello/pawn.py
@@ -1,10 +1,21 @@
 class Pawn:
+    """
+    Define a pawn, 0 for black, 1 for white
+
+    """
     def __init__(self,color):
+        """
+        Args:
+            color: 0 or 1
+        """
         if color!=0 or color!=1:
             raise ValueError(f"Color of Pawn should be 0 for black or 1 for white. The value {color} was given.")
         self.color=color
     
     def flip(self):
+        """
+        flip the pawn to the other color
+        """
         if self.color==1:
             self.color=0
         else:
