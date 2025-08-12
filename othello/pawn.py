30387af23f644889a604af53ef01cbda7d8c25ef
Thibault Launois <thibault.launois@posteo.net>
2025-08-12 10:33:24 +0200

    Protecte Pawn.color

--- othello/pawn.py
+++ othello/pawn.py
@@ -9,14 +9,18 @@ class Pawn:
             color: 0 or 1
         """
         if color!=0 and color!=1:
-            raise ValueError(f"Color of Pawn should be 0 for black or 1 for white. The value {color} was given.")
-        self.color=color
+            raise ValueError(f"Color of Pawn should be 0 for black or 1 for white. The value \"{color}\" was given.")
+        self.__color=color
+
+    @property
+    def color(self):
+        return self.__color
     
     def flip(self):
         """
         flip the pawn to the other color
         """
-        if self.color==1:
-            self.color=0
+        if self.__color==1:
+            self.__color=0
         else:
-            self.color=1
+            self.__color=1
