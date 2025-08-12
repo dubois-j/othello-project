946cc37168b4ef61d9d912659581a5fcc3406b78
J. Dubois <julienhm.dubois@gmail.com>
2025-08-12 10:29:35 +0200

    feat: Add a new Pawn

--- othello/board.py
+++ othello/board.py
@@ -33,7 +33,60 @@ class Board:
         board = np.reshape(board, (8,8))
 
         # Placing center Pawns
-        board[3,3], board[4,4] = Square(Pawn(1))
-        board[3,4], board[4,3] = Square(Pawn(0))
+        board[3,3].pawn, board[4,4].pawn = Pawn(1)
+        board[3,4].pawn, board[4,3].pawn = Pawn(0)
 
         self.board = board
+
+    
+    def addPawn(self, row, col, color):
+        """
+        If the move is valid, adds a Pawn of given color to the given position.
+        """
+
+        if self.canAddPawn(col, row, color):
+            self.board[row, col] = Pawn(color)
+            self.flipPawns(row, col, color)
+
+
+    def canAddPawn(self, row, col, color):
+        pass
+
+
+    def isSquareEmpty(self, row, col):
+        pass
+
+
+    def isFlipPossible(self, row, col, color):
+        """
+        Checks if placing a Pawn on the Square given by col and row will flip at least one other Pawn.
+
+        Args:
+            col (int): column index
+            row (int): row index
+            color (int): color of the Pawn to place.
+                         O : Black
+                         1 : White
+        
+        Returns:
+            (Bool): True if at least one Pawn will be flipped, False otherwise.
+        """
+
+        pass
+    
+
+    def flipPawns(row, col, color):
+
+
+
+    def displayBoard(self):
+        """
+        Displays the current state of the board. Black Pawns are displayed as *, white Pawns as O.
+
+        Args:
+
+        Returns:
+
+        """
+
+    
