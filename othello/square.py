f3269a6ac2a5b1ceb9d6ea048374df2405df6d1e
Thibault Launois <thibault.launois@posteo.net>
2025-08-12 10:28:47 +0200

    Protecte square.pawn attribute

--- othello/square.py
+++ othello/square.py
@@ -1,4 +1,8 @@
+import pawn as pa
 class Square:
     """A square on the board to contain None or a PAwn object"""
     def __init__(self,pawn):
-        self.pawn=pawn
+        if pawn==None or isinstance(pawn,pa.Pawn):
+            self.pawn=pawn
+        else:
+            raise AttributeError("You didn't gave a None or Pawn object to Square.")
