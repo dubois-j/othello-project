ab8016ad1bcefd2a4bf0e3c156be9bea307bcd23
Thibault Launois <thibault.launois@posteo.net>
2025-08-14 10:55:11 +0200

    feat: implement ai_player_random
    ai_player_random working

--- othello/ai_player_random.py
+++ othello/ai_player_random.py
@@ -1,5 +1,9 @@
 from othello.player import Player
+import random
+import time
 
 class AIPlayerRandom(Player):
-    def __init__(self):
-        pass
+    def getMove(self, rownames, colnames, possibleMoves):
+        print("Random player turn.")
+        #time.sleep(0.5)
+        return possibleMoves[random.randrange(len(possibleMoves))]
